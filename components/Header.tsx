9eeb5a8ea38ffe5c397e03a8635b6b766a90196e
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-05-02 16:04:56 +0200

    Adjust vertical padding

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { IoMdNotificationsOutline, IoMdPerson } from "react-icons/io";
 
 const Header: React.FC = () => {
     return (
-        <header className="bg-[#910655] hidden md:flex justify-between items-center px-16 py-4 ">
+        <header className="bg-[#910655] hidden md:flex justify-between items-center px-16 py-2 ">
             <Link href="/login" className="text-white">Login/ Sign Up</Link>
             <div className="flex gap-4 text-2xl text-white">
                 <IoMdNotificationsOutline />
